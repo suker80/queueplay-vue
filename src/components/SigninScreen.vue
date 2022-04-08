7017e9568277ff582aacc80238b7730f61ae7cec
suker <suker800@gmail.com>
2022-04-08 14:14:26 +0900

    feat: navbar

---
+++ src/components/SigninScreen.vue
@@ -0,0 +1,95 @@
+<template>
+    <main class="form-signin">
+        <div class="card">
+               <div class="card-body">
+                    <form class="form" @submit.prevent="submitLogin"> 
+                        <h2 class="h3 mb-3 fw-normal text-center">Please sign in</h2>
+
+                        <div class="form-group">
+                            <label>Email address</label>
+                            <input type="email" class="form-control form-control-lg" placeholder="Email address" v-model="email"/>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" class="form-control form-control-lg" placeholder="Password" v-model="password"/>
+                        </div>
+
+                        <div class="checkbox mb-3">
+                             <label><input type="checkbox" value="remember-me"> Remember me</label>
+                        </div>
+                        <div class="form-group">
+                        <button class="w-100 btn btn-lg btn-primary" type="submit" to="/signup">Sign in</button>
+                        </div>
+                        <div>
+                            <a href="http://localhost:8080/oauth2/authorization/google">
+                            <img src= '../assets/btn_google_signin_light_focus_web@2x.png'/>
+                            </a> 
+                        </div>
+                        
+                    </form>
+                </div>
+        </div>  
+        <p class="mt-3 mb-3 text-muted text-center">© 2020–2025</p>
+    </main>      
+</template>
+
+<script>
+    import axios from 'axios'
+    export default {
+        data() {
+            return {
+                email:'',
+                password:'',
+                name:'',
+                isLogin :false
+
+
+            }
+        },
+        methods: {
+            submitLogin: function(event){
+                event.preventDefault();
+                let self = this
+                
+                console.log(this.email,this.password);
+                var data = {
+                    email:this.email,
+                    password:this.password
+                }
+                axios.post('/login',data)
+                .then(function(response){
+                    if (response.status == 200){
+                        data.name = response.data.name;
+                        data.isLogin = true
+                        self.$emit('login',data)
+                        self.$router.push('/')
+
+
+                    }})
+                
+
+            }
+            
+        }
+        ,
+
+    }
+</script>
+
+<style lang="css">
+    body {
+        padding-bottom: 60px;        
+        align-items: center;
+    }
+    .form-signin {
+        padding-top: 120px;
+        height: 100%;
+        width: 100%;
+        max-width: 450px;
+        margin: auto;
+    }
+    label {
+        font-weight: 600;
+    }
+</style>
---
+++ src/components/SignupScreen.vue
@@ -0,0 +1,77 @@
+<template>
+    <main class="form-signin">
+        <div class="card">
+               <div class="card-body">
+                    <form class="form" @submit.prevent="submitjoin"> 
+                        <h2 class="h3 mb-3 fw-normal text-center">Please sign up</h2>
+
+
+                        <div class="form-group">
+                            <label>Email address</label>
+                            <input type="email" class="form-control form-control-lg" placeholder="Email address" />
+                        </div>
+
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" class="form-control form-control-lg" placeholder="Password"/>
+                        </div>                        
+
+                        <button class="w-100 btn btn-lg btn-primary" type="submit" to="/signin">Sign up</button>
+                    </form>
+                </div>
+        </div>  
+        <p class="mt-3 mb-3 text-muted text-center">© 2020–2025</p>
+    </main>      
+</template>
+
+<script>
+import axios from 'axios'
+    export default {
+        data() {
+            return {
+                email:'',
+                password:''
+                
+            }
+        },
+        methods: {
+            submitjoin:function(event){
+
+                event.preventDefault();
+                let self = this
+                
+                console.log(this.email,this.password);
+                var data = {
+                    email:this.email,
+                    password:this.password
+                }
+                axios.post("/join",data)
+                .then(function (response){
+                    console.log(response)
+                    if (response.status==200){
+                        self.$router.push('/')
+                    }})
+                .catch(function(response){
+                    console.log(response)
+                })
+            }
+            
+        }
+    }
+</script>
+
+<style lang="css">
+    body {
+        padding-top: 60px;
+        padding-bottom: 60px;        
+        align-items: center;
+    }
+    .form-signin {
+        width: 100%;
+        max-width: 450px;
+        margin: auto;
+    }
+    label {
+        font-weight: 600;
+    }
+</style>
